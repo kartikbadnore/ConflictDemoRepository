5ab67ff5506aa46023013339a6e65993633cadd9
kartik <nospam@gmail.com>
2019-07-17 17:30:12 +0530

    commited first

--- ConflictDemo/src/confictDemo1/ConflictDemo1.java
+++ ConflictDemo/src/confictDemo1/ConflictDemo1.java
@@ -5,11 +5,11 @@ public class ConflictDemo1 {
 	public static void main(String[] args) {
 		
 		
-		
-		for(int i=0;i<4;i++)
+		String [] cricketplayers= {"sachin","dhoni","rahul","rohit"};
+		for(int i=0;i<cricketplayers.length;i++)
 		{
 			
-			System.out.println("Conflict Demo 1");
+			System.out.println(cricketplayers[i]);
 		}
 		
 	}
