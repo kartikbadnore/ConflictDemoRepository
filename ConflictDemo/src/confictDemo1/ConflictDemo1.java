58f6d12d9970531e02fbb504b53900ada004cd88
kartik <nospam@gmail.com>
2019-07-17 16:55:32 +0530

    Commit in 1

---
+++ ConflictDemo/src/confictDemo1/ConflictDemo1.java
@@ -0,0 +1,17 @@
+package confictDemo1;
+
+public class ConflictDemo1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for(int i=0;i<4;i++)
+		{
+			
+			System.out.println("Conflict Demo 1");
+		}
+		
+	}
+	
+}
